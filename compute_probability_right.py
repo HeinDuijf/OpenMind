8fe15d06181caed8e53eccb189c38930a53c640f
Bram Kasteel <bjhkasteel@gmail.com>
2022-11-22 12:36:57 +0100

    Some renames

--- compute_probability_right.py
+++ compute_probability_right.py
@@ -39,30 +39,30 @@ class ComputeProbabilityRight:
     def probability_right_even_degree(self):
         # I win in two events:
         # (a) exactly half of the neighbors are correct and I am correct or
-        p_me_correct_and_half_rest = self.competence_associate * binom.pmf(
+        p_me_correct_causing_win = self.competence_associate * binom.pmf(
             self.degree_open_mindedness / 2,
             self.degree_open_mindedness,
             self.probability_companion_right,
         )
         # (b) more than half of the neighbors are correct
-        p_more_than_half_rest_correct = binom.sf(
+        p_win_without_me = binom.sf(
             self.degree_open_mindedness / 2,
             self.degree_open_mindedness,
             self.probability_companion_right,
         )
-        return p_me_correct_and_half_rest + p_more_than_half_rest_correct
+        return p_me_correct_causing_win + p_win_without_me
 
     def probability_right_uneven_degree(self):
         # I win in three events:
         # (a) exactly half of the neighbors plus 1 are correct and I am correct
-        p_me_correct_and_half_rest = self.competence_associate * binom.pmf(
+        p_me_causing_win = self.competence_associate * binom.pmf(
             (self.degree_open_mindedness + 1) / 2,
             self.degree_open_mindedness,
             self.probability_companion_right,
         )
 
         # (b) more than half of the neighbors plus 1 are correct
-        p_more_than_half_rest_correct = binom.sf(
+        p_win_without_me = binom.sf(
             (self.degree_open_mindedness + 1) / 2,
             self.degree_open_mindedness,
             self.probability_companion_right,
@@ -71,11 +71,7 @@ class ComputeProbabilityRight:
         # (c) there's a tie and the random choice is correct
         p_tie_random_choice_correct = 0.5 * self.p_tie()
 
-        return (
-            p_me_correct_and_half_rest
-            + p_more_than_half_rest_correct
-            + p_tie_random_choice_correct
-        )
+        return p_me_causing_win + p_win_without_me + p_tie_random_choice_correct
 
     def accuracy_information(self):
         probability_right = (
